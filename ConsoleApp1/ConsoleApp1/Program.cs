366856fff190d54c9b2547238b6df313bbc80bf4
DaniPlancha <104681854+DaniPlancha@users.noreply.github.com>
2023-02-25 20:51:50 +0200

    added interview exercise about calculating math expressions

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static double calculate(char[] initialExpression)
+        {
+            double result = 0;
+            char lastOperationSymbol = '_';
+            for (int i = 0; i < initialExpression.Length; i++)
+            {
+                if (initialExpression[i] == '+' || initialExpression[i] == '-' || initialExpression[i] == '*')
+                {
+                    lastOperationSymbol = initialExpression[i];
+                }
+                else if (char.IsDigit(initialExpression[i]))
+                {
+                    if (i == 0)
+                    {
+                        result = initialExpression[i] - '0';
+                    }
+                    else if (lastOperationSymbol == '+')
+                    {
+                        result += initialExpression[i] - '0';
+                    }
+                    else if (lastOperationSymbol == '-')
+                    {
+                        result -= initialExpression[i] - '0';
+                    }
+                    else if (lastOperationSymbol == '*')
+                    {
+                        result *= initialExpression[i] - '0';
+                    }
+                }
+                else if (initialExpression[i] == '(')
+                {
+                    for (int j = initialExpression.Length - 1; j > i; j--)
+                    {
+                        if (initialExpression[j] == ')')
+                        {
+                            var a = calculate(initialExpression.ToList().GetRange(i + 1, j - (i + 1)).ToArray());
+                            if (i == 0)
+                            {
+                                result = a;
+                            }
+                            else if (lastOperationSymbol == '+')
+                            {
+                                result += a;
+                            }
+                            else if (lastOperationSymbol == '-')
+                            {
+                                result -= a;
+                            }
+                            else if (lastOperationSymbol == '*')
+                            {
+                                result *= a;
+                            }
+                            i = j;
+                            break;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            //    (5*3)+1    //    1+(7*8)*5
+            Console.WriteLine(calculate(Console.ReadLine().ToCharArray()));
+        }
+    }
+}
